318d2804734d52c31fb285940131f337fe94f61e
sylee8048 <water8048@naver.com>
2025-06-29 20:50:49 +0900

    feat: type 정리

---
+++ src/types/index.d.ts
@@ -0,0 +1,13 @@
+interface ResCategory {
+  value: string;
+  label: string;
+}
+
+interface ResLogin {
+  accessToken: string;
+  refreshToken: string;
+}
+interface ResRefresh {
+  accessToken: string;
+  refreshToken: string;
+}
